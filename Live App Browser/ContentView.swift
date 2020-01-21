21f2ec75a8e5562e47696e346699e38e70e640fd
Joe Hinkle <joehinkle11@gmail.com>
2020-01-20 19:20:15 -0500

    Initial Commit

---
+++ Live App Browser/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Live App Browser
+//
+//  Created by Joseph Hinkle on 1/20/20.
+//  Copyright © 2020 Joseph Hinkle. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
